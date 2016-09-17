dce997674bd218b8c3c8eea53626c5390c43963a
Bradford Ware <bware8@gatech.edu>
2016-09-16 20:53:22 -0400

    added landing page and other pages as templates

--- serve.py
+++ serve.py
@@ -1,17 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_restful import Api
 #import api as restapi
 #api.py was another file that contained the class resources below
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 app.debug = True
 api = Api(app)
 
-
 @app.route('/')
 def index():
-	return render_template("index.html")
+	return render_template('index.html')
+
+@app.route('/admin')
+def admin():
+	return render_template('admin.html')
+
+@app.route('/requester')
+def requester():
+	return render_template('requester.html')
+
+@app.route('/taker')
+def taker():
+	return render_template('taker.html')
 
 #example set up from my last project
 #api.add_resource(restapi.LoginResource, '/api/login')
