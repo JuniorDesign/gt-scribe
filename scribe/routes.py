21f9a443283e9ce77e96d01af953a435915d3bb5
Sara Cagle <cagle.sara@gmail.com>
2016-09-18 18:42:13 -0400

    created user model, setting up api

--- scribe/routes.py
+++ scribe/routes.py
@@ -1,7 +1,7 @@
 #handles the routing for our application running on the local server (see serve.py in the root-level directory)
 
 from scribe import db
-#from scribe.rest import api as scribe_api
+from scribe.rest import api as scribe_api
 from scribe import app
 from flask import render_template
 from flask_restful import Api
@@ -25,6 +25,7 @@ def taker():
 	return render_template('taker.html')
 
 #example set up from my last project
-#api.add_resource(restapi.LoginResource, '/api/login')
+api.add_resource(scribe_api.HelloWorld, '/api/helloworld') #example of making the api
+api.add_resource(scribe_api.GetPassword, '/api/hacked')
 #api.add_resource(restapi.ReservationResource, '/api/reservation/<string:reservation_id>') #includes update_reservation
 #api.add_resource(restapi.UpdateReservationConfirmResource, '/api/reservation/<string:reservation_id>/availability')
