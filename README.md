f8235127ac152388955cb7cba7f1f6e779418e9f
Sara Cagle <cagle.sara@gmail.com>
2016-09-23 00:07:31 -0400

    can now register new users to db, doesnt check for duplicates or anything.

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ Authors: Sara Cagle, Hosna Chaudhry, Sneh Munshi, Jessica Rosenfield, Brad Ware
 3. Ensure you have Flask; you can use Pip3 to install it. `p3ip install Flask` and flask_restful, `pip3 install flask_restful`, `pip3 install flask-SQLAlchemy` (and anything else it asks you to install.)
 4. Run `python3 serve.py`
 5. If it prompts you about accepting any incoming connections, be sure to allow it.
-6. In your browser, navigate to `localhost:5000` to see the rendered index page.
+6. In your Internet browser, navigate to `localhost:5000` to see the rendered index page.
 7. For working with the database, we recommend you download DB Browser for SQLite, which can be found [here](http://sqlitebrowser.org/). In the SQLite Browser, open the database (`temp.db`, inside the `data` folder) to view and directly edit its contents. Be sure to select "Write Changes" to save changes.
 
 **This readme is dynamic and frequently undergoes changes.**
--- scribe/rest/api.py
+++ scribe/rest/api.py
@@ -1,6 +1,9 @@
 from scribe import db
 from flask_restful import Resource
+from flask_restful.reqparse import RequestParser
+from scribe.model.user import User
 from scribe.repositories.userRepository import UserRepository
+
 
 
 class HelloWorld(Resource):
@@ -10,13 +13,13 @@ class HelloWorld(Resource):
 class GetPassword(Resource):
 	def __init__(self):
 		self.userRepository = UserRepository()
-		variableName = userRepository().as_dict()
 	def get(self):
-		print(self.userRepository.find(1))
+		user = self.userRepository.find(1);
+		return user.as_dict();
 
 class UserRegistration(Resource):
 	def __init__(self):
-		self.reqparse = reqparse.RequestParser()
+		self.reqparse = RequestParser() #this parses the JSON files that get posted via ajax
 		self.reqparse.add_argument('username', type=str, required= True, help="GaTech Username is required to register", location='json')
 		self.reqparse.add_argument('password', type=str, required= True, help="Password is required to register", location='json')
 		self.reqparse.add_argument('firstName', type=str, required= True, help="First name is required to register", location='json')
@@ -26,18 +29,34 @@ class UserRegistration(Resource):
 
 	def post(self): 
 		args = self.reqparse.parse_args()
+		#Taking the information from the registration form and assinging it to Python variables
 		username = args['username']
 		password = args['password']
 		firstName = args['firstName']
 		lastName = args['lastName']
-		accountType = args['type']
-		approved = True
+		approved = True #approve users by default at this point
 
-		#todo: post to db w this data
+		if args['type'] == "admin":
+			accountType = "ADMIN"
+		elif args['type'] == "requester":
+			accountType = "REQUESTER"
+		elif args['type'] == "taker":
+			accountType = "TAKER"
+		else: #this is how you set a response json and a response status code
+			return {"error": "Account type is missing"}, 400
 
-		#if not db.mysqldb.user_exists(username) and  not db.mysqldb.email_exists(email):
-		#	db.mysqldb.register_user(username, email, password, firstName, lastName)
-		#	return {"message": "User successfully created", "result": True}
-		#else:
-		#return {"error": "This user already exists", "result": False}, 400
+		#once we have a working db here, first check if the user exists or not
+		#currently assuming all users are brand new
+		userRepository = UserRepository()
+		newUser = User(username, password, firstName, lastName, accountType, approved)
+		userRepository.add_or_update(newUser)
+		userRepository.save_changes()
+		print("user has been added to the db!")
+
+		return {"message": "Post to database was successful. New user registered."}
+		#if not userRepository.user_exists(user['email']):
+          #	new_user = User(uid, user['first_name'], user['last_name'], user['email'])
+           #     user_repository.add_or_update(new_user)
+		#	user_repository.save_changes()
+
 
