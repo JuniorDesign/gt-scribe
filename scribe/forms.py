21bfcd41d5702a6cc83b2148b9f8dd931902d556
Jessica Rosenfield <rosenfield.jess@gmail.com>
2016-11-13 15:24:18 -0500

    Change tabs to spaces

--- scribe/forms.py
+++ scribe/forms.py
@@ -2,8 +2,8 @@ from flask_wtf import Form
 from wtforms import TextField, TextAreaField, SubmitField, validators, ValidationError
  
 class FeedbackForm(Form):
-	name = TextField("Name", [validators.Required("Please enter your name.")])
-	email = TextField("Email", [validators.Required("Please enter your email address."), validators.Email("Please ensure that the email address is valid.")])
-	subject = TextField("Subject", [validators.Required("Please enter a subject.")])
-	message = TextAreaField("Message", [validators.Required("Please enter some feedback.")])
-	submit = SubmitField("Send")
+    name = TextField("Name", [validators.Required("Please enter your name.")])
+    email = TextField("Email", [validators.Required("Please enter your email address."), validators.Email("Please ensure that the email address is valid.")])
+    subject = TextField("Subject", [validators.Required("Please enter a subject.")])
+    message = TextAreaField("Message", [validators.Required("Please enter some feedback.")])
+    submit = SubmitField("Send")
--- scribe/repositories/enrollmentRepository.py
+++ scribe/repositories/enrollmentRepository.py
@@ -10,19 +10,19 @@ class EnrollmentRepository(BaseRepository):
             return super(EnrollmentRepository, self).add_or_update(entity)
 
         def course_already_registered(self, username, course_id):
-        	enrollment = super(EnrollmentRepository, self).get(username = username, course_id = course_id)
-        	if len(enrollment) > 0:
-        		return True
-        	return False
+            enrollment = super(EnrollmentRepository, self).get(username = username, course_id = course_id)
+            if len(enrollment) > 0:
+                return True
+            return False
 
         def get_enrollments_of_opposite_type(self, userType, course_id):
-        	oppType = ""
-        	if userType == "REQUESTER":
-        		oppType = "TAKER"
-        	elif userType == "TAKER":
-        		oppType = "REQUESTER"
-        	else:
-        		return None
-        	enrollments = [enrollment for enrollment in Enrollment.query.filter(course_id == Enrollment.course_id).group_by(Enrollment.enrollment_id) if enrollment.user.type == oppType]
+            oppType = ""
+            if userType == "REQUESTER":
+                oppType = "TAKER"
+            elif userType == "TAKER":
+                oppType = "REQUESTER"
+            else:
+                return None
+            enrollments = [enrollment for enrollment in Enrollment.query.filter(course_id == Enrollment.course_id).group_by(Enrollment.enrollment_id) if enrollment.user.type == oppType]
 
-        	return enrollments
+            return enrollments
